316626da1d60bb8b3e6e1c92440c28622c19821a
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-12 12:48:56 +0200

    Add fields in bookingcontroller

--- src/Controller/Admin/BookingCrudController.php
+++ src/Controller/Admin/BookingCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Booking;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class BookingCrudController extends AbstractCrudController
@@ -12,14 +16,13 @@ class BookingCrudController extends AbstractCrudController
         return Booking::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->hideOnForm();
+        yield DateTimeField::new('startDate')->setColumns(8);
+        yield DateTimeField::new('endDate')->setColumns(8);
+        yield AssociationField::new('room')->setColumns(8);
     }
-    */
+    
 }
