8150d8dfd871c4e50ed42f420fcab522ac84c732
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-12 13:51:23 +0200

    Add timeslot validator response

---
+++ src/Response/TimeslotValidatorResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Response;
+
+use App\Entity\Booking;
+
+class TimeSlotValidatorResponse
+{
+    private bool $success;
+    private string $message;
+    private function __construct( bool $success, string $message)
+    {
+        $this->success = $success;
+        $this->message = $message;
+    }
+
+
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
