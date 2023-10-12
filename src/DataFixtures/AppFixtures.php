17aca07c7793a0a588f60226ee24a935b7e6e70c
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-12 11:40:10 +0200

    Add appfixtures to load json data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,47 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTime;
+use App\Entity\Room;
+use App\Entity\Booking;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $json = file_get_contents(__DIR__ . '/rooms.json');
+        $data = json_decode($json, true);
+
+        foreach($data as $roomData) {
+            $room = new Room();
+            $room->setName($roomData['name']);
+            $room->setCapacity($roomData['capacity']);
+            $manager->persist($room);
+        }
 
         $manager->flush();
+
+        $json = file_get_contents(__DIR__ . '/bookings.json');
+        $data = json_decode($json, true);
+
+        foreach($data as $bookingData){
+            $booking = new Booking();
+            $room = $manager->getRepository(Room::class)->find($bookingData['room_id']);
+
+            if($room === null){
+                continue;
+            }
+
+            $booking->setRoom($room);
+            $booking->setStartDate(new DateTime($bookingData['start_date']));
+            $booking->setEndDate(new DateTime($bookingData['end_date']));
+
+            $manager->persist($booking);
+        }
+
+        $manager->flush();
+ 
     }
 }
