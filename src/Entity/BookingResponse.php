6fe97835961796b8dc6bf08ecbc4bb6f5911c8d2
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-12 13:09:10 +0200

    Add bookingresponse model

---
+++ src/Entity/BookingResponse.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+class BookingResponse
+{
+    private ?Booking $booking;
+    private bool $success;
+    private string $message;
+    private function __construct(?Booking $booking, bool $success, string $message)
+    {
+        $this->booking = $booking;
+        $this->success = $success;
+        $this->message = $message;
+    }
+
+    public function getBooking(): ?Booking
+    {
+        return $this->booking;
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
