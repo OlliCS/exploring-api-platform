3a7718313c31460aec29856e9396d45770ce0054
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-12 16:19:11 +0200

    Clean up time validator

--- src/Validator/TimeSlotValidator.php
+++ src/Validator/TimeSlotValidator.php
@@ -3,8 +3,10 @@
 namespace App\Validator;
 
 use DateTime;
+use App\Response\TimeSlotValidationResponse;
 
-class TimeSlotValidator{
+class TimeSlotValidator 
+{
     private DateTime $startDate;
     private DateTime $endDate;
 
@@ -14,8 +16,57 @@ class TimeSlotValidator{
         $this->endDate = $endDate;
     }
 
-    public function validate() {
-        
+    public function validate() : TimeSlotValidationResponse
+    {
+        $validation = $this->validateDatesAreInTheFuture();
+        if (!$validation->isSuccess()) {
+            return $validation;
+        }
+
+        $validation = $this->validateStartDateIsBeforeEndDate();
+        if (!$validation->isSuccess()) {
+            return $validation;
+        }
+
+        return $this->validateDurationIsNotTooLong();
     }
+
+    private function validateDatesAreInTheFuture() : TimeSlotValidationResponse
+    {
+        $now = new DateTime();
+        $invalidDates = [];
     
+        if($this->startDate < $now) {
+            $invalidDates[] = "start date {$this->startDate->format('Y-m-d H:i:s')}";
+        }
+    
+        if($this->endDate < $now) {
+            $invalidDates[] = "end date {$this->endDate->format('Y-m-d H:i:s')}";
+        }
+
+        if(!empty($invalidDates)) {
+            return new TimeSlotValidationResponse(false, "Invalid dates: " . implode(', ', $invalidDates));
+        }
+        return new TimeSlotValidationResponse(true, "Dates are in the future");
+    }
+
+    private function validateStartDateIsBeforeEndDate() : TimeSlotValidationResponse
+    {
+        if ($this->startDate > $this->endDate) {
+            return new TimeSlotValidationResponse(false, "The start date {$this->startDate->format('Y-m-d H:i:s')} cannot be greater than the end date {$this->endDate->format('Y-m-d H:i:s')}");
+        }
+        return new TimeSlotValidationResponse(true, "Start date is before end date");
+    }
+
+    private function validateDurationIsNotTooLong() : TimeSlotValidationResponse
+    {
+        $maxBookingDurationInSeconds = 12 * 60 * 60; // 12 hours
+        $diffInSeconds = $this->endDate->getTimestamp() - $this->startDate->getTimestamp();
+        
+        if ($diffInSeconds > $maxBookingDurationInSeconds) {
+            return new TimeSlotValidationResponse(false, "The booking cannot be longer than 12 hours");
+        }
+
+        return new TimeSlotValidationResponse(true, "The timeslot is valid");
+    }
 }
