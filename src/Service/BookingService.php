b4626e32a467edbb7b2354dba59e79fa94f7f325
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-12 13:52:12 +0200

    Implement better response  for availability errors

--- src/Service/BookingService.php
+++ src/Service/BookingService.php
@@ -2,9 +2,12 @@
 
 namespace App\Service;
 use DateTime;
+use Exception;
 use App\Entity\Room;
 use App\Entity\Booking;
+use App\Response\BookingResponse;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Response\TimeSlotValidatorResponse;
 
 class BookingService 
 {
@@ -15,40 +18,68 @@ class BookingService
         $this->entityManager = $entityManager;
     }
 
-    public function createBooking(Room $room, DateTime $startDate, DateTime $endDate): Booking
+    public function createBooking(Room $room, DateTime $startDate, DateTime $endDate): BookingResponse
     {
-        
+        try{
+            $timeSlotValidator = $this->isRoomAvailable($room, $startDate, $endDate);
 
+            if (!$timeSlotValidator->isSuccess()) {
+                return new BookingResponse(null, false, $timeSlotValidator->getMessage());
+            }
 
-
-        $booking = new Booking();
-        $booking->setRoom($room);
-        $booking->setStartDate($startDate);
-        $booking->setEndDate($endDate);
-
-        $this->entityManager->persist($booking);
-        $this->entityManager->flush();
-
-        return $booking;
+            $booking = new Booking();
+            $booking->setRoom($room);
+            $booking->setStartDate($startDate);
+            $booking->setEndDate($endDate);
+    
+            $this->entityManager->persist($booking);
+            $this->entityManager->flush();
+    
+            return new BookingResponse($booking, true, 'The booking has been created.');
+            
+        }
+        catch (Exception $e) {
+            return new BookingResponse(null, false, $e->getMessage());
+        }
     }
 
-    public function isRoomAvailable(Room $room, DateTime $startDate, DateTime $endDate): bool
+    public function isRoomAvailable(Room $room, DateTime $startDate, DateTime $endDate): TimeSlotValidatorResponse
     {
+        if (!$this->isStartDateBeforeEndDate($startDate, $endDate)) {
+            return new TimeSlotValidatorResponse(false, 'The time slot is not valid: start date must be before end date.');
+        }
+        
         $bookings = $this->entityManager->getRepository(Booking::class)->findBy(['room' => $room]);
 
         foreach ($bookings as $booking) {
+            $existingStart = $booking->getStartDate()->format('Y-m-d H:i:s');
+            $existingEnd = $booking->getEndDate()->format('Y-m-d H:i:s');
+            $desiredStart = $startDate->format('Y-m-d H:i:s');
+            $desiredEnd = $endDate->format('Y-m-d H:i:s');
+        
             if ($startDate >= $booking->getStartDate() && $startDate <= $booking->getEndDate()) {
-                return false;
+                return new TimeSlotValidatorResponse(false, 
+                    "Conflict with an existing booking ($existingStart to $existingEnd). " .
+                    "The desired start date ($desiredStart) is within this range."
+                );
             }
             if ($endDate >= $booking->getStartDate() && $endDate <= $booking->getEndDate()) {
-                return false;
+                return new TimeSlotValidatorResponse(false, 
+                    "Conflict with an existing booking ($existingStart to $existingEnd). " .
+                    "The desired end date ($desiredEnd) is within this range."
+                );
+            }
+            if ($startDate <= $booking->getStartDate() && $endDate >= $booking->getEndDate()) {
+                return new TimeSlotValidatorResponse(false, 
+                    "Conflict with an existing booking ($existingStart to $existingEnd). " .
+                    "The desired time slot ($desiredStart to $desiredEnd) envelops this booking."
+                );
             }
         }
-
-        return true;
+        return new TimeSlotValidatorResponse(true, 'The room is available.');
     }
 
-    public function isTimeSlotValid($startDate, $endDate): bool
+    public function isStartDateBeforeEndDate($startDate, $endDate): bool
     {
         return $endDate > $startDate;
     }
