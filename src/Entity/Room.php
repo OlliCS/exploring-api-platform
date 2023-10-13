e75e6cb2eef20cfc7ec6bbf4cbd5cdf6b37b7773
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-13 16:03:59 +0200

    Add more validation messages in room

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\RoomRepository;
@@ -13,7 +12,6 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
@@ -30,34 +28,33 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
     normalizationContext: ['groups' => ['room:read']],
     denormalizationContext: ['groups' => ['room:write']],
 )]
-#[UniqueEntity(fields: ['name'])]
+#[UniqueEntity(fields: ['name'] ,message: 'A room with this name already exists')]
 class Room
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-
     private ?int $id = null;
 
+
+
     #[ORM\Column(length: 50)]
-    #[Assert\NotBlank]
-    #[Assert\Length(min: 2, max: 50)]
-    #[Assert\Regex(pattern: '/^[a-zA-Z0-9]+$/')]
-
+    #[Assert\NotBlank(message: 'Fill in the name of the room')]
+    #[Assert\Length(min: 2, max: 50, minMessage: 'The minimum length of the name is 2 characters', maxMessage: 'The maximum length of the name is 50 characters')]
+    #[Assert\Regex(pattern: '/^[a-zA-Z0-9]+$/', message: 'The name must be alphanumeric')]
     #[Groups(['room:read', 'room:write'])]
-
-
     private ?string $name = null;
 
-    /**
-     * The capacity of the room in number of people.
-     */
+
+
     #[ORM\Column]
-    #[Assert\NotBlank]
-    #[Assert\GreaterThan(0)]
-    #[Assert\LessThan(100)]
+    #[Assert\NotBlank(message: 'Fill in capacity for the room')]
+    #[Assert\GreaterThan(1, message: 'Capacity must be greater than 1')]
+    #[Assert\LessThan(100, message: 'Capacity must be less than 100')]
     #[Groups(['room:read', 'room:write'])]
     private ?int $capacity = null;
+
+
 
     #[ORM\OneToMany(mappedBy: 'room', targetEntity: Booking::class, orphanRemoval: true)]
     #[Groups(['room:read'])]
@@ -65,10 +62,13 @@ class Room
 
 
 
-    public function __construct()
+    public function __construct($name, $capacity)
     {
+        $this->name = $name;
+        $this->capacity = $capacity;
         $this->bookings = new ArrayCollection();
     }
+
 
 
     public function getId(): ?int
