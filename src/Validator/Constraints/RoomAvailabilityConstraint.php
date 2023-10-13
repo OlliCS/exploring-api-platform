41b270192904e0df57cb98acd2d6081534e14be3
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-13 14:44:37 +0200

    Change to class validator

--- src/Validator/Constraints/RoomAvailabilityConstraint.php
+++ src/Validator/Constraints/RoomAvailabilityConstraint.php
@@ -2,12 +2,16 @@
 
 namespace App\Validator\Constraints;
 
+use Attribute;
 use Symfony\Component\Validator\Constraint;
 
-/**
- * @Annotation
- */
+#[Attribute]
 class RoomAvailabilityConstraint extends Constraint
 {
     public $responseMessage = '{{ responseMessage }}';
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
 }
