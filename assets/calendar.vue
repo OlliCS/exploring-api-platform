b6e6719061daeb37d370a59b9b9c1a3ed6831cc0
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-15 09:11:49 +0200

    Add the calendar to the page form daypilot

---
+++ assets/calendar.vue
@@ -0,0 +1,14 @@
+<template>
+  <DayPilotCalendar id="dp" />
+</template>
+
+<script>
+import {DayPilotCalendar} from '@daypilot/daypilot-lite-vue'
+
+export default {
+  name: 'Calendar',
+  components: {
+    DayPilotCalendar
+  },
+}
+</script>
