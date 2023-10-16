b119e8967d0a183e24a14ad96c6d09f0a4764afb
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-17 00:43:16 +0200

    composer require scheb/2fa-google-authenticator

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
