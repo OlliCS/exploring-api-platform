ce5861b6717445442374146b41b7ce9016d8d10b
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-17 03:50:04 +0200

    Get logged in user

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class SearchController extends AbstractController
 {
@@ -37,7 +38,7 @@ class SearchController extends AbstractController
     }
 
     #[Route("/api/bookings", name: "booking_create", methods: ["POST"])]
-    public function create(Request $request): Response
+    public function create(Request $request,UserInterface $userInterface ): Response
     {
         $requestData = json_decode($request->getContent(), true);
         $start = $requestData['startDate'];
@@ -48,6 +49,9 @@ class SearchController extends AbstractController
         
         $startDate = new DateTime($start);
         $endDate = new DateTime($end);
+
+        $user = $this->getUser();
+
 
         $bookingResponse = $this->bookingService->createBooking($room,$startDate, $endDate);
 
