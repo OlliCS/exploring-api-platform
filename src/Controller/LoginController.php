8952d3e4abeda7255c5cb178b0893cdf01ba8f0e
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-17 02:34:40 +0200

    Add login functionality

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('login/index.html.twig',[
             'error' => $authenticationUtils->getLastAuthenticationError(),
@@ -23,5 +24,12 @@ class LoginController extends AbstractController
             'target_path' => $this->generateUrl('app_login'),
             'translation_domain' => 'admin',
             'password_parameter' => 'password',]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(AuthenticationUtils $authenticationUtils): Response
+    {
+        throw new Exception('logout should never be reached');
+
     }
 }
