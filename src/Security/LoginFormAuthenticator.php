e6eff9db5002c1322f488d0fb76ca1fe267fdef1
Olivier Van Ingelghem <olivier.van.ingelghem@student.howest.be>
2023-10-17 02:52:16 +0200

    Add rule to  always login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -15,10 +15,11 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
-class LoginFormAuthenticator extends AbstractAuthenticator
+class LoginFormAuthenticator extends AbstractAuthenticator implements  AuthenticationEntryPointInterface
 {
     public function __construct(private UserRepository $userRepository,private RouterInterface $router)
     {
@@ -55,6 +56,7 @@ class LoginFormAuthenticator extends AbstractAuthenticator
                 return $user->getPassword() === $credentials;
             }, $password)
 
+
         );
 
 
@@ -81,14 +83,14 @@ class LoginFormAuthenticator extends AbstractAuthenticator
        
     }
 
-//    public function start(Request $request, AuthenticationException $authException = null): Response
-//    {
-//        /*
-//         * If you would like this class to control what happens when an anonymous user accesses a
-//         * protected page (e.g. redirect to /login), uncomment this method and make this class
-//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-//         *
-//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-//         */
-//    }
+   public function start(Request $request, AuthenticationException $authException = null): Response
+   {
+       /*
+        * If you would like this class to control what happens when an anonymous user accesses a
+        * protected page (e.g. redirect to /login), uncomment this method and make this class
+        * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+        *
+        * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+        */
+   }
 }
